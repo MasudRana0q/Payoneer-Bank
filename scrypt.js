ef997d6b5a1b457500372ace26514c22f620c638
MasudRana0q <masudsweetagro2008@gmail.com>
2025-02-10 23:15:21 +0600

    second time

---
+++ scrypt.js
@@ -0,0 +1,6 @@
+let submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", function(){
+    document.getElementById("loginContain").style.display = "none";
+    document.getElementById("trx").style.display = "block";
+})
